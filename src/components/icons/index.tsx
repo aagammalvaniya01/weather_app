7162a17f165ec947ab0e7a2f55e92a07a2b44b4f
aagammalvaniya01 <aagammalvaniya01@gmail.com>
2025-01-16 16:08:46 +0530

    Refactored code and folder structure

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,11 @@
+import { SmockIcon } from './SmockIcon';
+
+import { CloudsIcon } from './CloudsIcon';
+
+import { ClearIcon } from './ClearIcon';
+
+export const Icons = {
+	SmockIcon,
+	CloudsIcon,
+	ClearIcon,
+};
